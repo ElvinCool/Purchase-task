243cec443ca6f72f9e0bbf32085fa13627bcfe22
Ilya Ermak <47034408+ElvinCool@users.noreply.github.com>
2025-04-07 08:13:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,3 +22,49 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+const button = document.querySelector('.button');
+const cards = document.querySelectorAll('.card-item');
+
+  button.addEventListener('mouseenter', () => {
+    cards.forEach(card => {
+      card.classList.add('hovered-by-button');
+    });
+  });
+
+  button.addEventListener('mouseleave', () => {
+    cards.forEach(card => {
+      card.classList.remove('hovered-by-button');
+    });
+  });
+
+  const buttons = document.querySelectorAll('.card-button');
+
+  buttons.forEach(button => {
+    button.addEventListener('mouseenter', () => {
+      button.closest('.card-item').classList.add('hovered');
+    });
+  
+    button.addEventListener('mouseleave', () => {
+      button.closest('.card-item').classList.remove('hovered');
+    });
+  });
+
+
+  let timeLeft = 10 * 60; 
+
+  function updateTimer() {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    const timerDisplay = document.getElementById("timer");
+
+    timerDisplay.textContent = 
+      `${String(minutes).padStart(2, '0')} : ${String(seconds).padStart(2, '0')}`;
+
+    if (timeLeft > 0) {
+      timeLeft--;
+      setTimeout(updateTimer, 1000);
+    }
+  }
+
+  updateTimer();
